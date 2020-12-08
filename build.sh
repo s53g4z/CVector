bca828875bc74e3544df4669fc1e4b275c48a3ea
nobody <nobody@example.com>
2020-12-07 22:14:49 -0800

    Minor build.sh changes.

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-if ! [ -f ./tests/libCV.so ]; then
+if [ ! -f ./tests/libCV.so ]; then
+	printf "Building library ...";
 	cd ./implm &&
 	gcc -c -Wall -Werror -O2 -fpic CV.c &&
 	gcc -shared -o libCV.so CV.o &&
@@ -9,7 +10,10 @@ if ! [ -f ./tests/libCV.so ]; then
 		printf 'ERROR: could not cd out';
 		exit 2;
 	}
+	printf " done.\n";
 fi
 
+printf "Building test program ...";
 cd tests &&
-gcc -O0 -g -Wall -Wextra -o 1 main.c -L. -lCV -Wl,-rpath=.
+gcc -O0 -g -Wall -Wextra -o 1 main.c -L. -lCV -Wl,-rpath=. &&
+printf " done.\n";
